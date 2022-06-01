6ef592eac5badcf9d68bb60a43ba9776467e63ae
maripetrova <74290687+maripetrova@users.noreply.github.com>
2022-06-01 11:23:29 +0300

    fff

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'style.css';
 import App from 'App.js';
+import Main from 'Main.js';
 
 ReactDOM.render(
-    <App />,
+    <Main />,
   document.getElementById('root')
 );
